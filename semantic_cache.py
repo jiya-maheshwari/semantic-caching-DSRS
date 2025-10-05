ce6aea8b6bcb1c85d344ecf1862929ae0ea45028
jiya-maheshwari <jiyamaheshwari.10@gmail.com>
2025-10-05 03:57:18 -0500

    semantic similarity

--- semantic_cache.py
+++ semantic_cache.py
@@ -1,22 +1,61 @@
-import os 
-import redis 
+import os
+import redis
 import google.generativeai as genai
 from dotenv import load_dotenv
 import numpy as np
+from google.generativeai import types
+from sklearn.metrics.pairwise import cosine_similarity
 
 load_dotenv()
 
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost") 
+#connecting to redis 
+
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
-REDIS_USERNAME = os.getenv("REDIS_USERNAME", "Jiya")
-REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "")  
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "")
 
 redis_client = redis.Redis(
-  host=REDIS_HOST,
-  port=REDIS_PORT,
-  username=REDIS_USERNAME,
-  password=REDIS_PASSWORD
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    password=REDIS_PASSWORD,
+    decode_responses=False
 )
 
-redis_client.ping()
+#function to egenrate embeddings 
 
+import os
+import numpy as np
+import google.generativeai as genai
+from dotenv import load_dotenv
+from sklearn.metrics.pairwise import cosine_similarity
+
+load_dotenv()
+
+def semantic_similarity(texts):
+    genai.configure(api_key=os.getenv("GOOGLE_API_KEY")) 
+    
+    embeddings = []
+    for text in texts:
+        result = genai.embed_content(
+            model="models/text-embedding-004", 
+            content=text,
+            task_type="retrieval_document"
+        )
+        embeddings.append(result['embedding'])
+    
+    embeddings_matrix = np.array(embeddings)
+    similarity_matrix = cosine_similarity(embeddings_matrix)
+    return similarity_matrix
+
+texts = [
+    "What is the meaning of life?",
+    "What is the purpose of existence?",
+    "How do I bake a cake?"
+]
+
+result = semantic_similarity(texts)
+print(result)
+
+
+# class semanticCache:
+#     def __init__(self):
